b537c7600fa63121e4030231b13fd86ece33bd29
Raul <reudavel@gmail.com>
2021-01-25 23:02:04 -0600

    More responsive nav

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ console.log("Hello from webpack");
 import "./index.scss";//modify to use scss files
 
 
+//For active tabs
+let mainNavLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+    if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop){
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
+
 //For changue the navbar style on scroll
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
