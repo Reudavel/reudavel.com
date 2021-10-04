1363280e99affda64739d02e6105563bf6b73ec0
Raul <reudavel@gmail.com>
2021-10-03 21:11:10 -0500

    Fixing hamburger menu

--- src/index.js
+++ src/index.js
@@ -31,12 +31,15 @@ var navbar = document.getElementById("navbar");
   prevScrollpos = currentScrollPos;
 }
 
-//Show and hide hamburger menu
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
